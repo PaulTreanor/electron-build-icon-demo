603e04e2d52c310f79ae160c9629f29ae2b6e39d
paultreanor <paul.treanor@fourtheorem.com>
2023-01-28 20:40:28 +0000

    Fix index.html path in electron.js

--- public/electron.js
+++ public/electron.js
@@ -4,7 +4,7 @@ const url = require('url');
 
 function createWindow() {
     const startUrl = process.env.ELECTRON_START_URL || url.format({
-        pathname: path.join(__dirname, '../index.html'),    
+        pathname: path.join(__dirname, './index.html'),    
         protocol: 'file:',
         slashes: true,
     });
